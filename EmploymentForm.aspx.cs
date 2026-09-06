24027a40bd64d1e2c40ce8f73cea2048059ee8f1
Warren Sifre <warren@crosswaveit.com>
2026-09-06 16:20:43 -0400

    Made updates to DLL

--- EmploymentForm.aspx.cs
+++ EmploymentForm.aspx.cs
@@ -184,7 +184,7 @@ public partial class EmploymentForm : System.Web.UI.Page
                 string senderEmail = "application@indianapowdercoatings.com";
                 //string receiverEmail = "shauntwatts@gmail.com";
                 // string receiverEmail = "hreagan@indianapowdercoatings.com";
-                string receiverEmail = "tamoya.hopkins@crosswaveit.com";
+                string receiverEmail = "warren.sifre@crosswaveit.com";
                 var tenantId = "e8f1c112-d1ce-4d57-ab8d-8ec62e10bd2a";
                 var clientId = "9e032e88-7a42-4559-9d2f-b60362a719ff";
                 // 24 month (expires 7/22/2027) - new secret - Zx98Q~1KI6E0CuRYA5EUW_CsjItaycbAyeyTFblx
